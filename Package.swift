994a6f4f9c310da2007ec0a416367a23b0ce6056
Mattias Lundhaw <mattias.lundhaw@thnx.se>
2022-03-03 10:02:12 +0100

    Update package platform iOS to v15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftDate",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .watchOS(.v2), .tvOS(.v9)
+        .macOS(.v10_10), .iOS(.v15), .watchOS(.v2), .tvOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
